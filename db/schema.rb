cede6012741aad4cbc53d29bee042723deea2488
Daniel Nguyen <email": null>
2016-06-27 16:37:09 -0700

    made devise migration and models because we forgot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627231715) do
+ActiveRecord::Schema.define(version: 20160627233442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,26 @@ ActiveRecord::Schema.define(version: 20160627231715) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "league_admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "league_admins", ["email"], name: "index_league_admins_on_email", unique: true, using: :btree
+  add_index "league_admins", ["reset_password_token"], name: "index_league_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "leagues", force: :cascade do |t|
     t.integer  "league_admin_id"
@@ -49,6 +69,26 @@ ActiveRecord::Schema.define(version: 20160627231715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "team_admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "team_admins", ["email"], name: "index_team_admins_on_email", unique: true, using: :btree
+  add_index "team_admins", ["reset_password_token"], name: "index_team_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "team_games", force: :cascade do |t|
     t.integer  "away_team_id"
