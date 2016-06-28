0d16695e2827dc68df0d0852ee7fc389ef54f3df
Jacqueline Phan <jtphan14@gmail.com>
2016-06-28 12:41:41 -0700

    Display all players on trade page

---
+++ app/assets/javascripts/trade.js
@@ -0,0 +1,10 @@
+$( document ).ready(function(){
+
+  //Upon click, display list of players
+  $('#inside').on('change', '.proposing_team_trade', function(){
+
+    // var data =  $(this).val(),
+    // var proposing_team = @team.find(data),
+
+  })
+})
