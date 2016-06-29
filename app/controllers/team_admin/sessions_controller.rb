f1ad6a53d199e95b5eea9248f1b469bdea21dd11
Danny Pham <email": null>
2016-06-29 15:05:58 -0700

    fixed league admin tools to approve user to team

--- app/controllers/team_admin/sessions_controller.rb
+++ app/controllers/team_admin/sessions_controller.rb
@@ -2,13 +2,12 @@ class TeamAdmin::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    redirect_to new_session_path
+  end
 
   # POST /resource/sign_in
   # def create
-
   #   # super
   #   redirect_to '/teams'
   # end
