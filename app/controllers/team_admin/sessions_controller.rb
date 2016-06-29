0c43af96ee68128ff51e12e75e593fa08fc41243
Danny Pham <email": null>
2016-06-29 15:16:25 -0700

    renamed a couple of links

--- app/controllers/team_admin/sessions_controller.rb
+++ app/controllers/team_admin/sessions_controller.rb
@@ -7,18 +7,21 @@ class TeamAdmin::SessionsController < Devise::SessionsController
   end
 
   # POST /resource/sign_in
-  # def create
-  #   # super
-  #   redirect_to '/teams'
-  # end
+  def create
+    # super
+    redirect_to '/teams'
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
+  def after_sign_in_path_for(resource)
+    team_path(resource)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
