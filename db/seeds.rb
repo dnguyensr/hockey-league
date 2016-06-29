1610d03bff623a6b0218d1eee6fc67de6aa89338
Danny Pham <email": null>
2016-06-29 13:00:16 -0700

    changed seed file for easier league admin login

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 LeagueAdmin.create({
-  first_name:   Faker::Name.first_name,
-  last_name:    Faker::Name.last_name,
-  email:        Faker::Internet.email,
+  first_name:   'Kevin',
+  last_name:    'Solorio',
+  email:        'ksolo@dbc.com',
   password:     "password",
   })
 
