6912279af447841bea0ddd92e5396e383dbcfff0
Richie Yi <helloworldrichie@gmail.com>
2016-06-29 14:48:39 -0700

    Fix a tag links to stand out more and remove secondary color team styling

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
   var mainColor = $('#primary_color').text();
-  var secondaryColor = $('#secondary_color').text();
   $("body").css("background-color", mainColor);
-  $('body').css("color", secondaryColor);
 });
