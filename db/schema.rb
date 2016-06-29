103ca18cff9243dd5e12ea4c2ac47e76d48a303f
Richie Yi <helloworldrichie@gmail.com>
2016-06-29 10:26:49 -0700

    Add position to Player model and relative show pages

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160628232823) do
   create_table "players", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "position"
     t.string   "jersey_number"
     t.integer  "team_id"
     t.datetime "created_at",    null: false
