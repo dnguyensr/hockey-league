fd8267b5e2d2f689b2bda2a94b9212e6156abe84
Danny Pham <email": null>
2016-06-28 17:24:29 -0700

    add new approved column to teamadmin for league admin approval

--- app/models/team_admin.rb
+++ app/models/team_admin.rb
@@ -5,4 +5,20 @@ class TeamAdmin < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :teams
   has_many :posts
+
+  def full_name
+    self.first_name.capitalize + " " + self.last_name.capitalize
+  end
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super
+    end
+  end
 end
