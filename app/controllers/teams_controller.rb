34166e8d907c072ff227385df925c7c947b7df5a
Jacqueline Phan <jtphan14@gmail.com>
2016-06-29 12:23:27 -0700

    Add team admin functionality for trades on show page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,8 @@ class TeamsController < ApplicationController
   def show
     @posts = Post.filter_by_team(params[:id])
     @games = Game.filter_by_team(params[:id])
-    @proposedtrades = Trade.where(accepting_team: @team.id)
+    @reviewabletrades = Trade.where(accepting_team: @team.id)
+    @proposedtrades = Trade.where(proposing_team: @team.id)
   end
 
   # GET /teams/new
